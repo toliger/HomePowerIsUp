8fc205e2cd0830174d73e6dc41a0e0cd1a0c68b7
oligertimothee <fraxy.gfx@gmail.com>
2018-01-19 20:27:27 +0100

    patch

--- app/app.js
+++ app/app.js
@@ -5,12 +5,10 @@ var freq_refresh = 15;
 
 var host =	'home.oligertimothee.fr';
 
-var port = 80;
-
-request.get('https://'+ host + '/setSignalUp?uuid=test');
+request.get(`https://${host}/setSignalUp?uuid=test`);
 
 setInterval(() => {
-	request.get('https://'+ host + '/setSignalUp?uuid=test');
-}, 30 * 60 * 1000);
+	request.get(`https://${host}/setSignalUp?uuid=test`);
+}, freq_refresh * 60 * 1000);
 
 console.log('app start');
